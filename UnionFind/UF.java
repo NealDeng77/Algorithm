87ed9adc656d77782f4788e56b19f04f6d4fb9bf
NealDeng77 <nealdeng77@gmail.com>
2019-11-15 13:06:15 -0800

    union find

---
+++ UnionFind/UF.java
@@ -0,0 +1,11 @@
+public class UF {
+	// initialize union-find data structure with N objects
+	UF(int N)
+	
+	//add connection between p and q
+	void union(int p, int q)
+	
+	//are p and q in the same component?
+	boolean connected(int p, int q)
+	
+}
