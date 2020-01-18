2650b11c82f73638cef1c8a7519c9f86e1741f6f
nealdeng77 <nealdeng77@gmail.com>
2020-01-18 12:17:07 -0800

    stackofplates

--- StackQueueBags/StackOfPlates.java
+++ StackQueueBags/StackOfPlates.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 
 /*
  * Cracking the coding interview. 3.3 stack of Plates
@@ -13,16 +14,75 @@ import java.util.ArrayList;
  */
 public class StackOfPlates {
 	private int currentStack;
-	private ArrayList<Stack> stacks = new ArrayList<Stack>();
+	private ArrayList<FixedCapacityStack> stacks = new ArrayList<FixedCapacityStack>();  //store the stacks
+	private final static int CAPACITY = 10;
 	
+	/*
+	 * constructor
+	 */
+	public StackOfPlates(){
+		FixedCapacityStack<Integer> stack = new FixedCapacityStack<Integer>(CAPACITY);
+		stacks.add(stack);
+	}
+
+	/*
+	 * Push item to the stack
+	 */
 	public void push(int item) {
-		
+		int numOfStacks = stacks.size();
+		FixedCapacityStack<Integer> stack = (FixedCapacityStack<Integer>)stacks.get(numOfStacks - 1);
+		if(!stack.isFull() && stack != null) {
+			stack.push(item);
+		}else {
+			FixedCapacityStack<Integer> newStack = new FixedCapacityStack<Integer>(CAPACITY);
+			newStack.push(item);
+			stacks.add(newStack);
+		}
 	}
 	
+	/*
+	 * Pop from the stacks
+	 * @throw EmptyStackException if all stacks are empty
+	 */
 	public int pop() {
-		
+		if(stacks.size() == 0) {
+			throw new EmptyStackException();
+		}
+		int numOfStacks = stacks.size();
+		FixedCapacityStack<Integer> stack = (FixedCapacityStack<Integer>)stacks.get(numOfStacks - 1);
+		//pop from the last stack
+		int tempItem = stack.pop();
+		//if the stack is empty, remove that stack
+		if(stack.isEmpty()) {
+			stacks.remove(numOfStacks - 1);
+		}
+		return tempItem;
 	}
 	
+//	/*
+//	 * Pop from a specific sub-stack
+//	 * We assume some stacks can be not at full capacity. In this situation, the popAt() operation
+//   * has less time complexity
+//	 */
+//	public int popAt(int index) {
+//		FixedCapacityStack<Integer> stack = (FixedCapacityStack<Integer>)stacks.get(index - 1);
+//		if(stack.isEmpty() || stack == null) {
+//			throw new EmptyStackException();
+//		}
+//		int tempItem = stack.pop();
+//		//if the stack is empty, remove that stack
+//		if(stack.isEmpty()) {
+//			stacks.remove(index - 1);
+//		}
+//		return tempItem;
+//	}
+	
+	/*
+	 * Pop from a specific sub-stack
+	 * It's like a "rollover" system. If we pop an element from stack 1, we need to remove
+	 * the bottom of stack 2 and push it onto stack 1. We then need to rollover from stack
+	 * 3 to stack 2, stack 4 to stack 3, etc.
+	 */
 	public int popAt(int index) {
 		
 	}
