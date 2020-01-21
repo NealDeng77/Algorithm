b733ac7c59ff8ab9e97025189b135b07e608e5ad
nealdeng77 <nealdeng77@gmail.com>
2020-01-20 17:51:15 -0800

    doubly linked list

--- LinkedList/DoubleLinkedList.java
+++ LinkedList/DoubleLinkedList.java
@@ -3,20 +3,58 @@ public class DoubleLinkedList {
 	private Node head;
 	private class Node{
 		Node next;
-		int value;
+		int data;
+		Node prev;
+		public Node(int d){
+			this.data = d;
+		}
 	}
 	
-	public Node deleteNode(Node head, int value) {
+	/*
+	 * Add a node at the front of the list
+	 */
+	public void push(int data) {
+		Node oldhead = head;
+		head = new Node(data);
+		head.next = oldhead;
+		head.prev = null;
+		oldhead.prev = head;
+	}
+	
+	/*
+	 * Add a node after a given node
+	 */
+	public void insertAfter(Node prevNode, int data) {
+		if(prevNode == null) {
+			System.out.println("The given previous node can't be null");
+		}
+		//allocate node
+		Node newNode = new Node(data);
+		prevNode.next.prev = newNode;
+		newNode.prev = prevNode;
+		newNode.next = prevNode.next;
+		prevNode.next = newNode;
+	}
+	
+	/*
+	 * Append a node at the end of the list
+	 */
+	public void append(int data) {
+		Node newNode = new Node(data);
+	}
+	
+	
+	public Node deleteNode(Node head, int data) {
 		if(head == null) {
 			return null;
 		}
 		//remove head
-		if(head.value == value) {
+		if(head.data == data) {
 			return head.next;
 		}
 		Node x = head;
 		while(x.next != null) {
-			if(x.next.value == value) {
+			if(x.next.data == data) {
 				x.next = x.next.next;
 				return head;
 			}
