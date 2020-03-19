3939bd2a4689580cb7111bbfd96f8cd0065e81c7
NealDeng77 <nealdeng77@gmail.com>
2020-03-19 15:47:52 -0700

    graphAPi

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -1,31 +1,46 @@
+import edu.princeton.cs.algs4.Bag;
+
 // Graph api
+//Adjacency-list graph representation, maintain vertex-indexed array of lists
 public class Graph {
+	private Bag<Integer>[] adj;     //using bag to implement the graph
+	private final int V;            //number of vertices
+	
 	/*
 	 * Create an empty graph with V vertices
 	 */
-	Graph(int V) {
-		
+	@SuppressWarnings("unchecked")
+	public Graph(int V) {
+		this.V = V;
+		//create empty graph with V vertices
+		adj = (Bag<Integer>[])new Bag[V];
+		for(int v = 0; v < V; v++) {
+			adj[v] = new Bag<Integer>();
+		}
 	}
 	
-	/*
-	 * Create  a graph from input stream
-	 */
-	Graph(In in) {
-		
-	}
+//	/*
+//	 * Create  a graph from input stream
+//	 */
+//	public Graph(In in) {
+//		
+//	}
 	
 	/*
 	 * Add an edge v-w
 	 */
-	void addEdge(int v, int w) {
-		
+	public void addEdge(int v, int w) {
+		//add edge v-w
+		adj[v].add(w);
+		adj[w].add(v);
 	}
 	
 	/*
 	 * Vertices adjacent to v
 	 */
-	Iterable<Integer> adj(int v) {
-		
+	public Iterable<Integer> adj(int v) {
+		//iterator for vertices adjacent to v
+		return adj[v];
 	}
 	
 	/*
