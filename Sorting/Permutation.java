a3928f338c5563c27f49940a51dd1f16a8101a5e
nealdeng77 <nealdeng77@gmail.com>
2020-04-11 06:32:56 -0700

    permutation

---
+++ Sorting/Permutation.java
@@ -0,0 +1,11 @@
+//Permutation. Given two integer arrays of size nn, design a subquadratic algorithm to 
+//determine whether one is a permutation of the other. That is, do they contain exactly 
+//the same entries but, possibly, in a different order.
+
+/*
+ * The same idea as intersection and union or two arrays.
+ * First sort two arrays, then check the length, then check each elements
+ */
+public class Permutation {
+
+}
