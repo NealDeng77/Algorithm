502f749ea0c2d26a06085f8059841dd4208ce585
helena <helenawang77@gmail.com>
2020-04-12 08:01:48 -0700

    bottom up merge sort

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -1,6 +1,6 @@
 
 public class MergeSort {
-	private Comparable[] aux;
+	private static Comparable[] aux;
 	private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
 		for(int i = lo; i <= hi; i++) {
 			aux[i] = a[i];
@@ -8,27 +8,6 @@ public class MergeSort {
 		
 		int i = lo, j = mid + 1;
 		for(int k = lo; k <= hi; k++) {
-//			if(aux[i].compareTo(aux[j]) <= 0) {
-//				a[lo] = aux[i];
-//				i++;
-//				lo++;
-//			} else {
-//				a[lo] = aux[j];
-//				j++;
-//				lo++;
-//			}
-//			if(i < mid) {
-//				for(int x = i; i < mid + 1; i++) {
-//					a[lo] = aux[i];
-//					lo++;
-//				}
-//			}
-//			if(j < hi) {
-//				for(int x = j; j < hi; j++) {
-//					a[lo] = aux[j];
-//					lo++;
-//				}
-//			}
 			if(aux[i].compareTo(aux[j]) <= 0 ) a[k] = aux[i++];
 			else if(i > mid) a[k] = aux[j++];
 			else if(j > hi) a[k] = aux[i++];
@@ -38,9 +17,19 @@ public class MergeSort {
 	
 	private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
 		if(hi <= lo) return;
+		//improvement: if it's a small array, just use the insertion sort because it doesn't need
+		// the extra array and move between arrays and it performs well when the array is partially sorted
+		if(hi <= lo + (CUTOFF - 1)) {
+			InsertionSort.sort(a, lo, hi);
+			return;
+		}
 		int mid = lo + (hi - lo) / 2;
 		sort(a, aux, lo, mid);
 		sort(a, aux, mid + 1, hi);
+		//Improvement: if the array is already sorted, return
+		if(a[mid + 1].compareTo(a[mid]) >= 0) {
+			return;
+		}
 		merge(a, aux, lo, mid, hi);
 	}
 	
