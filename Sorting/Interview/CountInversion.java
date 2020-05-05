e98080fed71192dbc372fd8f43e5455f3b304bd0
NealDeng77 <nealdeng77@gmail.com>
2020-05-04 18:47:05 -0700

    count inversions

--- Sorting/Interview/CountInversion.java
+++ Sorting/Interview/CountInversion.java
@@ -1,44 +1,67 @@
 package Interview;
 //reference: https://www.geeksforgeeks.org/counting-inversions/
 //reference : https://medium.com/@ssbothwell/counting-inversions-with-merge-sort-4d9910dc95f0
-//reference : https://www.cs.princeton.edu/~wayne/kleinberg-tardos/pdf/05DivideAndConquerI.pdf
+//reference : https://www.cs.princeton.edu/~wayne/kleinberg-tardos/pdf/05DivideandConquerI.pdf
 public class CountInversion {
-//	static int local = 0;
-    static int global = 0;
-
-    static void merge(int[] A, int[] B, int lo, int mid, int hi) {
-        for(int i = lo; i <= hi; i++) {
-            B[i] = A[i];
+    
+	// a[lo, mid]  and a[mid + 1, hi] is already sorted.
+	public static long merge(int[] a, int[] aux, int lo, int mid, int hi) {
+        long inversion = 0;
+    	for(int i = lo; i <= hi; i++) {
+            aux[i] = a[i];
         }    
         int i = lo;
         int j = mid + 1;
         for(int k = lo; k <=hi; k++) {
-            if(i > mid) A[k] = B[j++];
-            else if(j > hi) A[k] = B[i++];
-            else if(B[i] <= B[j]) A[k] = B[i++];
+            if(i > mid) a[k] = aux[j++];
+            else if(j > hi) a[k] = aux[i++];
+            else if(aux[i] <= aux[j]) a[k] = aux[i++];
+            //if a[i] > a[j], all the elements left in the subarray a[lo, mid]
+            //are greater than a[j], so that inversions exist, add them on.
             else{
-                global += mid - i + 1;
-                A[k] = B[j++];
+                inversion += mid - i + 1;
+                a[k] = aux[j++];
             }
         }
+        return inversion;
     }
     
-    static void sort(int[] A, int[] B, int lo, int hi) {
-        if(hi <= lo) return;
-        int mid = lo + (hi - lo) / 2;
-        sort(A, B, lo, mid);
-        sort(A, B, mid + 1, hi);
-        merge(A, B, lo, mid, hi);
-    }
+	public static long count(int[] a) {
+		//use a new copied array b to avoid sorting the original array a
+		int[] b = new int[a.length];
+		int[] aux = new int[a.length];
+		for(int i = 0; i < a.length - 1; i++) {
+			b[i] = a[i];
+		}
+		long inversions = count(a, b, aux, 0, a.length - 1);
+		return inversions;
+	}
+	
+	
+	private static long count(int[] a, int[] b, int[] aux, int lo, int hi) {
+		long inversions = 0;
+		if(hi <= lo) return 0;
+		int mid = lo + (hi - lo) / 2;
+		inversions += count(a, b, aux, lo, mid);
+		inversions += count(a, b, aux, mid + 1, hi);
+		inversions += merge(b, aux, lo, mid, hi);
+		assert inversions == brute(a, lo, hi);
+		return inversions;
+	}
     
-    static int sort(int[] A) {
-        int[] B = new int[A.length];
-        sort(A, B, 0, A.length - 1);
-        return global;
-    }
+	private static long brute(int[] a, int lo, int hi) {
+		long inversions = 0;
+		for(int i = lo; i < hi; i++) {
+			for(int j = i + 1; j < hi; j++) {
+				if(a[j] < a[i]) inversions++;
+			}
+		}
+		return inversions;
+	}
+    
 	
 	public static void main (String[] args) {
 	    int[] a = {2, 4, 1, 3, 5};
-		System.out.println(sort(a));
+		System.out.println(count(a));
 	}
 }
